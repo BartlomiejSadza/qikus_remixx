82614def107f75b696354f482f879dd691e867da
BartlomiejSadza <bartlomiej.sadza@icloud.com>
2024-09-14 16:19:45 +0200

    App opener and styles

---
+++ src/components/hello.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Hello() {
+  return (
+    <div className="hello">
+      <h2>Quikś remix by Bartlomiej Sadza</h2>
+      <p>Wanna try check your knowledge?</p>
+      <button>Start Quiz!</button>
+    </div>
+  );
+}

2b6d1dd2d75b60bb6bfbf1e365ed3178c53e11db
BartlomiejSadza <bartlomiej.sadza@icloud.com>
2024-09-14 17:30:29 +0200

    new branch

--- src/components/quiz.js
+++ src/components/quiz.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from "react";
 import he from "he";
 
+
+
 export default function Quiz(props) {
 
 
   return (
     <div>
-      <h1>Quiz</h1>
       <p>{props.question}</p>
-        
     </div>
   );
 }
@@ -26,36 +26,3 @@ export default function Quiz(props) {
 //       'Argumentum ad populum'
 //     ]
 //   },
-//   {
-//     type: 'multiple',
-//     difficulty: 'easy',
-//     category: 'General Knowledge',
-//     question: 'What company developed the vocaloid Hatsune Miku?',
-//     correct_answer: 'Crypton Future Media',
-//     incorrect_answers: [ 'Sega', 'Sony', 'Yamaha Corporation' ]
-//   },
-//   {
-//     type: 'multiple',
-//     difficulty: 'easy',
-//     category: 'General Knowledge',
-//     question: 'Which of the following blood component forms a plug at the site of injuries?',
-//     correct_answer: 'Platelets',
-//     incorrect_answers: [ 'Red blood cells', 'White blood cells', 'Blood plasma' ]
-//   },
-//   {
-//     type: 'multiple',
-//     difficulty: 'easy',
-//     category: 'General Knowledge',
-//     question: 'Who invented the first ever chocolate bar, in 1847?',
-//     correct_answer: 'Joseph Fry',
-//     incorrect_answers: [ 'Andrew Johnson', 'John Cadbury', 'John Tyler' ]
-//   },
-//   {
-//     type: 'multiple',
-//     difficulty: 'easy',
-//     category: 'General Knowledge',
-//     question: 'What is the first book of the Old Testament?',
-//     correct_answer: 'Genesis',
-//     incorrect_answers: [ 'Exodus', 'Leviticus', 'Numbers' ]
-//   }
-// ]
